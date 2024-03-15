ae745cd72f560184aaf91a005654b42cf877cb3f
Kolisang <kolisang9@gmail.com>
2024-03-15 21:23:27 +0200

    Add routes

--- portfolio-project-2024/src/App.js
+++ portfolio-project-2024/src/App.js
@@ -1,10 +1,29 @@
-
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
+import Home from './containers/home'
+import About from './containers/about'
+import Contact from './containers/contact'
+import Resume from './containers/resume'
+import Skills from './containers/skills'
+import Portfolio from './containers/portfolio'
 
 function App() {
   return (
     <div className="App">
+      {/* particles */}
 
+      {/* navBar */}
+
+      {/* main page */}
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/skills' element={<Skills/>} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+ 
     </div>
   );
 }
